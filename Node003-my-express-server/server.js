cb18136930f5a3d97fb5cd48710c79237b5d8abf
Nick2099 <75340386+Nick2099@users.noreply.github.com>
2021-01-26 21:44:53 +0100

    Update server.js

--- Node003-my-express-server/server.js
+++ Node003-my-express-server/server.js
@@ -5,11 +5,19 @@ const app = express();
 
 app.get("/", function(req, res) {
   // console.log(request);
-  res.send("<h1>Hello</h1>");
+  res.send("<h1>Hello</h1><h2>Try to write as a address:</h2><h2>localhost:3000/contact</h2><h2>localhost:3000/about</h2>");
 });
 
 // localhost:3000 is represented by "/"
 // When that request happen than we can trigger our callback function can have 2 parameters: request and response
+
+app.get("/contact", function(req, res) {
+  res.send("Contact me at: ....");
+});
+
+app.get("/about", function(req, res) {
+  res.send("My name is Nikica Dadic");
+});
 
 app.listen(3000, function() {
   console.log("Server started on Port 3000");
