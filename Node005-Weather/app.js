64065ca069d2993e19878cfd16c0517305290f7b
Nick2099 <75340386+Nick2099@users.noreply.github.com>
2021-02-03 18:03:48 +0100

    Update app.js
    Getting data from API using JSON

--- Node005-Weather/app.js
+++ Node005-Weather/app.js
@@ -6,11 +6,29 @@ const app = express();
 app.get("/", function(req, res) {
   const url = "https://api.openweathermap.org/data/2.5/weather?q=Landshut&units=metric&appid=0d4382fe20d4ecee43dd65eaebc16cf0";
   https.get(url, function(response) {
-    console.log(response);
+    console.log(response.statusCode);
+    response.on("data", function(data) {
+      const weatherData = JSON.parse(data);
+      console.log(weatherData);
+      const temp = weatherData.main.temp;
+      const place = weatherData.name;
+      const land = weatherData.sys.country;
+      const descript = weatherData.weather[0].description;
+
+      console.log("Weather in " + place + ", " + land + ":");
+      console.log(temp + "°C, " + descript);
+
+      /* example of making object and turning in into JSON string */
+      const object = {
+        name: "Nikica",
+        favouriteColor: "indigo",
+        height: "1.76"
+      }
+      console.log(JSON.stringify(object));
+    });
   });
   res.send("Server is up and running.");
 });
-
 
 
 
