3fb030e6dcdf9959e46623e351336c6db8c9499c
Nick2099 <75340386+Nick2099@users.noreply.github.com>
2021-02-06 00:33:05 +0100

    Selecting data from mySQL

--- Node006-P01/app.js
+++ Node006-P01/app.js
@@ -3,13 +3,23 @@ const express = require("express");
 const https = require("https");   // ?
 const http = require("http");     // ?
 const bodyParser = require("body-parser");
-const mysql = require("mysql");
+var mysql = require("mysql");
 const fs = require("fs");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 // end of required lines
+
+// constants for program
+const myUserTable = "users";
+// end of constants
+
+// variables for program
+var sql = "";
+var userId = 0;
+// end of variables
+
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
@@ -18,18 +28,68 @@ app.get("/", function(req, res) {
 app.post("/", function(req, res) {
   var emailAddress = req.body.email;
   var password = req.body.pswd;
+  var forwarded = req.body.submit;
+  console.log("reg: " + forwarded);
 
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("<h2>E-mail address: " + emailAddress + "</h2><h2>Password: " + password + "</h2>");
+  res.write("<h2>E-mail address: " + emailAddress + "</h2><h2>Password: " + password + "</h2><h2>Task: " + forwarded + "</h2");
 
   let rawData = fs.readFileSync("C:/Programming/settings.json");
   let infoLog = JSON.parse(rawData);
-  console.log(infoLog);
+  // console.log(infoLog.database);
+
+  var con = mysql.createConnection({
+    host: infoLog.database.host,
+    user: infoLog.database.user,
+    password: infoLog.database.password,
+    database: infoLog.database.database
+    // insecureAuth: true
+  });
+
+  if (forwarded == "login") {
+    try {
+      con.connect(function(err) {
+        if (err) throw err;
+        sql = "SELECT * FROM information_schema.tables WHERE table_schema = '" + con.config.database + "' AND table_name = '" + myUserTable + "'";
+        console.log(sql);
+        con.query(sql, function(err, result, fields) {
+          if (err) throw err;
+          console.log(result);
+          if (result.length == 1 ) {
+            // sql = "SELECT * FROM '" + myUserTable + "' WHERE user = '" + emailAddress + "' AND password = '" + password + "'";
+            sql = "SELECT id FROM " + myUserTable + " WHERE user = '" + emailAddress + "' AND password = '" + password + "'";
+            console.log(sql);
+            con.query(sql, function(err, result, fields) {
+              if (result.length==1) {
+                userId = result[0].id;
+                console.log("User ID: " + userId);
+              }
+              // console.log(result);
+              // console.log("Lenght: ", result.length);
+              // console.log("ID    : ", result[0].id);
+              // result1 = JSON.parse(JSON.stringify(result));
+              // console.log(result1);
+              // console.log(result1[0].id);
+            });
+          };
+        });
+      });
+    }
+    catch (err) {
+      console.log(err.message);
+    }
+  }
+
+  // con.connect(function(err) {
+  //   if (err) {
+  //     console.log("Error:" + err.message);
+  //   } else {
+  //     console.log("Connected to mySQL!");
+  //   };
+  // })
 
   res.send();
 });
-
-
 
 
 app.listen(3000, function() {
