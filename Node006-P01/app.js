327ec277eb0dd7bf5a8d5826453ea494f4c9a41f
Nick2099 <75340386+Nick2099@users.noreply.github.com>
2021-02-07 00:48:53 +0100

    Update app.js
    It looks that some parts of code are finished before other (getting data from mySQL file)! I have to find a way that following code can be executed after some functions ended.

--- Node006-P01/app.js
+++ Node006-P01/app.js
@@ -18,6 +18,7 @@ const myUserTable = "users";
 // variables for program
 var sql = "";
 var userId = 0;
+var reswritetxt = "";
 // end of variables
 
 
@@ -29,10 +30,10 @@ app.post("/", function(req, res) {
   var emailAddress = req.body.email;
   var password = req.body.pswd;
   var forwarded = req.body.submit;
-  console.log("reg: " + forwarded);
+  console.log("Forwarded: " + forwarded);
 
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("<h2>E-mail address: " + emailAddress + "</h2><h2>Password: " + password + "</h2><h2>Task: " + forwarded + "</h2");
+  // res.writeHead(200, {'Content-Type': 'text/html'});
+  // res.write("<h2>E-mail address: " + emailAddress + "</h2><h2>Password: " + password + "</h2><h2>Task: " + forwarded + "</h2");
 
   let rawData = fs.readFileSync("C:/Programming/settings.json");
   let infoLog = JSON.parse(rawData);
@@ -46,49 +47,89 @@ app.post("/", function(req, res) {
     // insecureAuth: true
   });
 
-  if (forwarded == "login") {
-    try {
-      con.connect(function(err) {
-        if (err) throw err;
-        sql = "SELECT * FROM information_schema.tables WHERE table_schema = '" + con.config.database + "' AND table_name = '" + myUserTable + "'";
-        console.log(sql);
-        con.query(sql, function(err, result, fields) {
-          if (err) throw err;
-          console.log(result);
-          if (result.length == 1 ) {
-            // sql = "SELECT * FROM '" + myUserTable + "' WHERE user = '" + emailAddress + "' AND password = '" + password + "'";
+  con.connect(function(err) {
+    if (err) {
+      console.log("Error 01:" + err.message);
+    } else {
+      console.log("Connected to mySQL!");
+      sql = "SELECT * FROM information_schema.tables WHERE table_schema = '" + con.config.database + "' AND table_name = '" + myUserTable + "'";
+      // console.log(sql);
+      con.query(sql, function(err, result) {
+        if (err) {
+          console.log("Error 02: " + err.message);
+        } else if (result.length > 0) {
+          if (forwarded == "login") {
             sql = "SELECT id FROM " + myUserTable + " WHERE user = '" + emailAddress + "' AND password = '" + password + "'";
-            console.log(sql);
+            // console.log(sql);
             con.query(sql, function(err, result, fields) {
-              if (result.length==1) {
-                userId = result[0].id;
-                console.log("User ID: " + userId);
-              }
-              // console.log(result);
-              // console.log("Lenght: ", result.length);
-              // console.log("ID    : ", result[0].id);
-              // result1 = JSON.parse(JSON.stringify(result));
-              // console.log(result1);
-              // console.log(result1[0].id);
+              if (err) {
+                console.log("Error 03: " + err.message);
+              } else {
+                if (result.length==1) {
+                  userId = result[0].id;
+                  reswritetxt = "User ID: " + userId;
+                } else if (result.length==0) {
+                  reswritetxt = "There is no such user.";
+                };
+              };
+              console.log("==>> " + reswritetxt);
             });
           };
-        });
+        } else {
+          console.log("Error 04: Table_scheme or table_name don't exist!");
+        };
       });
-    }
-    catch (err) {
-      console.log(err.message);
-    }
-  }
+    };
+    console.log("==>>> " + reswritetxt + "User ID: " + userId);
+  });
 
-  // con.connect(function(err) {
-  //   if (err) {
-  //     console.log("Error:" + err.message);
-  //   } else {
-  //     console.log("Connected to mySQL!");
+  function Output() {
+    console.log("==> " + reswritetxt + "User ID: " + userId);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("<h2>E-mail address: " + emailAddress + "</h2><h2>Password: " + password + "</h2><h2>Task: " + forwarded + "</h2>" + reswritetxt);
+    res.send();
+  };
+
+  setTimeout(Output, 1000); // without timedays function Output is executing before con.connect is finished! Have to find another way!!
+                            // =====> Callback functions <======
+
+  // if (forwarded == "login") {
+  //   try {
+  //     con.connect(function(err) {
+  //       if (err) throw err;
+  //       sql = "SELECT * FROM information_schema.tables WHERE table_schema = '" + con.config.database + "' AND table_name = '" + myUserTable + "x'";
+  //       console.log(sql);
+  //       con.query(sql, function(err, result, fields) {
+  //         console.log(result);
+  //         if (err) throw err;
+  //         // console.log(result);
+  //         if (result.length == 1 ) {
+  //           // sql = "SELECT * FROM '" + myUserTable + "' WHERE user = '" + emailAddress + "' AND password = '" + password + "'";
+  //           sql = "SELECT id FROM " + myUserTable + " WHERE user = '" + emailAddress + "' AND password = '" + password + "'";
+  //           console.log(sql);
+  //           try {
+  //             con.query(sql, function(err, result, fields) {
+  //               console.log(result);
+  //               if (result.length==1) {
+  //                 userId = result[0].id;
+  //                 console.log("User ID: " + userId);
+  //               } else if (result.length==0) {
+  //                 console.log("There is no such user.");
+  //               };
+  //             });
+  //           }
+  //           catch (err) {
+  //             console.log("Error 3: " + err.message);
+  //           }
+  //         };
+  //       });
+  //     });
   //   };
-  // })
+  //   catch (err) {
+  //     console.log("Error 1: " + err.message);
+  //   };
+  // };
 
-  res.send();
 });
 
 
