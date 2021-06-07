b6a557ef7553a445dfae68cb6b6aa51ae9efcd94
Nick2099 <75340386+Nick2099@users.noreply.github.com>
2021-06-07 21:31:37 +0200

    using key property for React component

--- Node013-React-DevTools/src/components/App.jsx
+++ Node013-React-DevTools/src/components/App.jsx
@@ -3,11 +3,27 @@ import Card from "./Card";
 import contacts from "../contacts";
 import Avatar from "./Avatar";
 
+function createCard(contact) {
+  return <Card 
+    key={contact.id} /* it have to be created like this and it's expected by React */
+    id={contact.id} /* so, if we need this information we can create another property */
+    name={contact.name}
+    img={contact.imgURL}
+    tel={contact.phone}
+    email={contact.email}    
+  />
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Avatar img="https://media-exp1.licdn.com/dms/image/C4D03AQHtmMum5pt9UA/profile-displayphoto-shrink_200_200/0/1546863149926?e=1627516800&v=beta&t=YJ5t21a3O5q65VhoBz9ZfHFGPHEZOOFszZnweL61IZU" />
+      {contacts.map(createCard)}
+
+      {/*the line above is replacing all the line below  */}
+
+      <h1 className="heading">Made without using map</h1>
       <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
