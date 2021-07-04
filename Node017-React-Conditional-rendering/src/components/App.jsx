9905f3238bf7385c8b881c91690369b1bec0f96a
Nick2099 <75340386+Nick2099@users.noreply.github.com>
2021-07-04 22:50:49 +0200

    Using Ternary Operator and &&

--- Node017-React-Conditional-rendering/src/components/App.jsx
+++ Node017-React-Conditional-rendering/src/components/App.jsx
@@ -12,7 +12,13 @@ function renderConditionally() {
 }
 
 function App() {
-  return <div className="container">{renderConditionally()}</div>
+  // instead of next line and calling a function renderConditionally
+  // return <div className="container">{renderConditionally()}</div>
+  // we can just write a Ternary Operator that includes all the logic from function renderConditionally
+  return <div className="container">{
+    isLoggedIn ? <h1>Hello</h1> : <Login />
+    // it can be written also with && operator - see the Notes
+  }</div>
 }
 
 export default App;
