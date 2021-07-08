45528372e38aac15020c5d4f977b0ee6018aafbe
Nick2099 <75340386+Nick2099@users.noreply.github.com>
2021-07-09 00:40:31 +0200

    useState practice

--- Node020-React-useState-hook-practice/src/components/App.jsx
+++ Node020-React-useState-hook-practice/src/components/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 
 function App() {
+  let time = new Date().toLocaleTimeString();
+  const [tmpTime, setTime] = React.useState(time);
+
+  function getTime() {
+    let newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+
+  setInterval(getTime, 200);
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{tmpTime}</h1>
+      <button onClick={getTime}>Get Time</button>
     </div>
   );
 }
