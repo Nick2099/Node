0084e3817aae1107df22c07738cfc458c17a4dd4
Nick2099 <75340386+Nick2099@users.noreply.github.com>
2021-07-20 00:28:01 +0200

    forms, useState

--- Node022-React-Forms/src/components/App.jsx
+++ Node022-React-Forms/src/components/App.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 
 function App() {
+
+  const [name, setName] = React.useState("");
+  const [headingText, setHeadingText] = React.useState("");
+  const [nameLenght, setNameLenght] = React.useState(0);
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleClick() {
+    setHeadingText(name);
+    setNameLenght(name.length);
+    console.log(name, name.length);
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {headingText}</h1>
+      <input 
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+        value={name} />
+      <button onClick={handleClick}>Submit</button>
+      <p>Lenght: {nameLenght}</p>
     </div>
   );
 }
