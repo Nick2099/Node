ba0baf8dece582018948e04ac60100b627b0481c
Nick2099 <75340386+Nick2099@users.noreply.github.com>
2021-07-21 00:40:33 +0200

    React Forms, preventDefault

--- Node022-React-Forms/src/components/App.jsx
+++ Node022-React-Forms/src/components/App.jsx
@@ -7,15 +7,42 @@ function App() {
   const [nameLenght, setNameLenght] = React.useState(0);
 
   function handleChange(event) {
+    setNameLenght(event.target.value.length);
     setName(event.target.value);
   }
 
+  function handleClick(event) {
+    setHeadingText(name);
+    setNameLenght(name.length);
+    console.log(name, name.length, event);
+    // next line prevent refreshing the page back to the default values
+    event.preventDefault();
+  }
+
+  return (
+    <div className="container">
+      <h1>Hello {headingText}</h1>
+      <form onSubmit={handleClick}>
+        <input 
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+          value={name} />
+        <button type="submit">Submit</button>
+      </form>
+      <p>Lenght: {nameLenght}</p>
+    </div>
+  );
+
+  {/*
   function handleClick() {
     setHeadingText(name);
     setNameLenght(name.length);
     console.log(name, name.length);
   }
+  */}
 
+  {/*
   return (
     <div className="container">
       <h1>Hello {headingText}</h1>
@@ -28,6 +55,8 @@ function App() {
       <p>Lenght: {nameLenght}</p>
     </div>
   );
+   */}
+
 }
 
 export default App;
